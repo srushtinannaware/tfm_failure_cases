1cc1d57df8a59084c3518de737586bf9cfa69ae2
Srushti Nannaware <srushtinannaware05@gmail.com>
2026-07-13 19:47:30 +0530

    Datasets Folder

---
+++ datasets/load_dataset.py
@@ -0,0 +1,19 @@
+"""
+Load real-world datasets.
+
+"""
+
+def load_dataset(name):
+    """
+    Load dataset.
+
+    Parameters
+    ----------
+    name : str
+
+    Returns
+    -------
+    X
+    y
+    """
+    pass
